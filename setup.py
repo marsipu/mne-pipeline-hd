75d6c606607681f9c3a22ff4bdb2e673e8c0a319
marsipu <beetlejuice@zwerge-musical.de>
2019-11-29 00:21:40 +0100

    Changes to setup.py, MANIFEST.in created

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup, find_packages
 
+setup(name='mne_pipeline_hd',
+      version='0.1',
+      description='A pipeline for brain-data analysis with MNE-Python',
+      url='https://github.com/marsipu/mne_pipeline_hd.git',
+      author='marsipu',
+      author_email='martin.schulz@stud.uni-heidelberg.de',
+      license='GPL-3.0',
+      packages=find_packages(exclude=['docs', 'tests']))
