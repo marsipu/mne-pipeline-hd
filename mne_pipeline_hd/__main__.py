973ca5622e4ae1360863de5ef8654303d0467437
marsipu <46245704+marsipu@users.noreply.github.com>
2020-08-23 11:11:30 +0200

    Compatibility for Spyder

--- mne_pipeline_hd/__main__.py
+++ mne_pipeline_hd/__main__.py
@@ -8,6 +8,8 @@ inspired by: https://doi.org/10.3389/fnins.2018.00006
 License: BSD (3-clause)
 """
 import sys
+from inspect import getsourcefile
+from os.path import abspath
 from pathlib import Path
 
 from PyQt5.QtCore import QTimer, Qt
@@ -20,8 +22,9 @@ try:
     # if installed via pip, currently always the pip installed package is loaded
     import mne_pipeline_hd
 except ModuleNotFoundError:
-    top_package_path = str(Path(sys.path[0]).parent)
-    sys.path.insert(0, top_package_path)
+    # Get the package_path, should work across platforms and in spyder
+    package_parent = str(Path(abspath(getsourcefile(lambda: 0))).parent.parent)
+    sys.path.insert(0, package_parent)
 
 from mne_pipeline_hd.gui import main_window
 from mne_pipeline_hd.gui.qt_utils import StderrStream, StdoutStream
@@ -65,6 +68,9 @@ def main():
     timer = QTimer()
     timer.timeout.connect(lambda: mw)
     timer.start(500)
+
+    # For Spyder to make console accessible again
+    app.lastWindowClosed.connect(app.quit)
 
     sys.exit(app.exec())
 
