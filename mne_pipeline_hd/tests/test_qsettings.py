f08ec052a46e79dfeb3d5cc424e05dfb106627d6
marsipu <dev@earthman-music.de>
2022-08-11 13:46:04 +0100

    cleanup tests

--- mne_pipeline_hd/tests/test_qsettings.py
+++ mne_pipeline_hd/tests/test_qsettings.py
@@ -8,20 +8,13 @@ License: GPL-3.0
 """
 
 import json
-import sys
 from importlib import resources
 
 from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QApplication
 
 
-def test_qsettings_types():
+def test_qsettings_types(qtbot):
     """Test if QSettings keep types on all operating systems."""
-    app = QApplication.instance()
-    if not app:
-        app = QApplication(sys.argv)
-    app.setApplicationName('test')
-
     with resources.open_text('mne_pipeline_hd.resource',
                              'default_settings.json') as file:
         default_qsettings = json.load(file)['qsettings']
@@ -29,12 +22,6 @@ def test_qsettings_types():
     if len(QSettings().childKeys()) == 0:
         for v in default_qsettings:
             QSettings().setValue(v, default_qsettings[v])
-        test_qsettings_types()
-        # ToDo: This test as for now is only sensitive after been
-        #   run at least twice because in one run the values are
-        #   apparently somehow cached. (maybe better with pytest-qt)
-        app.quit()
-        del app
 
     for v in default_qsettings:
         value = QSettings().value(v)
