660679163eb46c5aaa0fc7a91276515b461388fe
marsipu <dev@mgschulz.de>
2023-07-27 11:21:44 +0200

    fix version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def parse_version():
     ) as fid:
         for line in (line.strip() for line in fid):
             if line.startswith("__version__"):
-                version = line.split("=")[1].strip().strip("'")
+                version = line.split("=")[1].strip().strip("\"")
                 break
     if version is None:
         raise RuntimeError("Could not determine version")
