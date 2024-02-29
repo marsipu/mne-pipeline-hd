aaca63f57c40d7ff387ed26ad5cccd71f8e86d7b
Martin Schulz <dev@mgschulz.de>
2024-02-29 18:25:42 +0100

    start implementing nodes basics

--- mne_pipeline_hd/development/nodes_demo.py
+++ mne_pipeline_hd/development/nodes_demo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 
+from NodeGraphQt.base.factory import NodeFactory
 from qtpy.QtWidgets import QApplication, QMainWindow
 from NodeGraphQt import (
     BaseNode,
@@ -12,30 +13,66 @@ from NodeGraphQt import (
 from mne_pipeline_hd.gui.base_widgets import CheckList
 
 
-class InputWidgetWrapper(NodeBaseWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+class PipeNodeFactory(NodeFactory):
+    """This overrrides the NodeFactory to insert a Controller-object
+    as an argument to node-creation."""
 
-        self.set_name("input")
-        self.set_label("Select Input-Data")
-        self.set_custom_widget(CheckList(["data1", "data2", "data3"]))
+    def __init__(self, ct):
+        self.ct = ct
+        super().__init__()
+
+    def create_node_instance(self, node_type=None):
+        if node_type in self.aliases:
+            node_type = self.aliases[node_type]
+
+        _NodeClass = self.nodes.get(node_type)
+        if _NodeClass:
+            return _NodeClass(self.ct)
+
+
+class PipeNodeBaseWidget(NodeBaseWidget):
+    """Base class for all node-widgets in the pipeline.
+    Since pipeline is not making use of the property system,
+    this class makes cicumvention easier."""
+
+    def __init__(self, parent=None, name=None, label=None):
+        super().__init__(parent, name, label)
 
     def get_value(self):
-        return 1
+        return
 
     def set_value(self, value):
         pass
 
 
-class InputNode(BaseNode):
-    __identifier__ = "input"
-    NODE_NAME = "Input Node"
+class PipeBaseNode(BaseNode):
+    """Base class for all nodes in the pipeline.
+    This simplifies some functionality and adds some custom methods.
+    """
+
+    __identifier__ = "pipeline_nodes"
+    NODE_NAME = "NodeName"
 
     def __init__(self):
         super().__init__()
 
-        self.input_widget = InputWidgetWrapper(self.view)
-        self.add_custom_widget(self.input_widget, tab="input")
+    def add_widget(self, widget, widget_type=None, tab=None):
+        wrapper_widget = PipeNodeBaseWidget(self.view, "widget_name", "widget_label")
+        wrapper_widget.set_custom_widget(widget)
+        super().add_custom_widget(wrapper_widget, widget_type, tab=tab)
+
+
+class InputNode(PipeBaseNode):
+    def __init__(self, ct):
+        super().__init__()
+        meeg_list = CheckList(
+            [1, 2, 3],
+            [1, 2],
+            ui_button_pos="top",
+            show_index=True,
+            title="Select MEG/EEG",
+        )
+        self.add_widget(meeg_list, tab="input")
 
         self.add_output("Data", multi_output=True)
 
@@ -44,7 +81,7 @@ class FunctionNode(BaseNode):
     __identifier__ = "function"
     NODE_NAME = "Function Node"
 
-    def __init__(self):
+    def __init__(self, ct):
         super().__init__()
 
         self.add_input("Data-In 1")
@@ -57,11 +94,11 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.graph = NodeGraph()
+        self.graph = NodeGraph(node_factory=PipeNodeFactory(self))
         self.setCentralWidget(self.graph.widget)
         self.graph.register_nodes([InputNode, FunctionNode])
 
-        input_node = self.graph.create_node("input.InputNode")
+        input_node = self.graph.create_node("pipeline_nodes.InputNode")
         func_node = self.graph.create_node("function.FunctionNode")
         func_node2 = self.graph.create_node("function.FunctionNode")
 
