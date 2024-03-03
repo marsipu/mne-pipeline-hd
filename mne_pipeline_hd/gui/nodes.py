830bc38dbcd686765a6edbadf048ab273a3278c1
Martin Schulz <dev@mgschulz.de>
2024-03-03 21:26:57 +0100

    first implementation into mainwindow

--- mne_pipeline_hd/gui/nodes.py
+++ mne_pipeline_hd/gui/nodes.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from NodeGraphQt import NodeBaseWidget, BaseNode
+from NodeGraphQt import NodeBaseWidget, BaseNode, NodeGraph
 from NodeGraphQt.base.factory import NodeFactory
 
-from gui.base_widgets import CheckList
+from mne_pipeline_hd.gui.base_widgets import CheckList
 
 
 class PipeNodeFactory(NodeFactory):
@@ -17,7 +17,7 @@ class PipeNodeFactory(NodeFactory):
         if node_type in self.aliases:
             node_type = self.aliases[node_type]
 
-        _NodeClass = self.__nodes.get(node_type)
+        _NodeClass = self.nodes.get(node_type)
         if _NodeClass:
             return _NodeClass(self.ct)
 
@@ -45,10 +45,11 @@ class PipeBaseNode(BaseNode):
     __identifier__ = "pipeline_nodes"
     NODE_NAME = "NodeName"
 
-    def __init__(self):
+    def __init__(self, ct):
+        self.ct = ct
         super().__init__()
 
-    def add_widget(self, widget, widget_label, widget_type=None, tab=None):
+    def add_widget(self, widget, widget_type=None, tab=None):
         wrapper_widget = PipeNodeBaseWidget(self.view, "widget_name")
         wrapper_widget.set_custom_widget(widget)
         super().add_custom_widget(wrapper_widget, widget_type, tab=tab)
@@ -56,7 +57,7 @@ class PipeBaseNode(BaseNode):
 
 class InputNode(PipeBaseNode):
     def __init__(self, ct):
-        super().__init__()
+        super().__init__(ct)
         meeg_list = CheckList(
             ct.pr.all_meeg,
             ct.pr.sel_meeg,
@@ -67,3 +68,32 @@ class InputNode(PipeBaseNode):
         self.add_widget(meeg_list, tab="input")
 
         self.add_output("Data", multi_output=True)
+
+
+class FunctionNode(PipeBaseNode):
+    def __init__(self, ct):
+        super().__init__(ct)
+
+        self.add_input("Data-In 1")
+        self.add_input("Data-In 2")
+
+        self.add_output("Data-Out", multi_output=True)
+
+
+class PipeNodeGraph(NodeGraph):
+    def __init__(self, ct):
+        self.ct = ct
+        node_factory = PipeNodeFactory(self.ct)
+        super().__init__(node_factory=node_factory)
+        self.register_nodes([InputNode, FunctionNode])
+
+        input_node = self.create_node("pipeline_nodes.InputNode")
+        func_node = self.create_node("pipeline_nodes.FunctionNode")
+        func_node2 = self.create_node("pipeline_nodes.FunctionNode")
+
+        input_node.set_output(0, func_node.input(0))
+        func_node2.set_input(1, func_node.output(0))
+
+        self.auto_layout_nodes()
+        self.clear_selection()
+        self.fit_to_selection()
