5b537aaf075bac5b2cca51778d16679e6458e1ee
Martin Schulz <dev@mgschulz.de>
2024-05-13 22:00:58 +0200

    remove MANIFEST.in

--- mne_pipeline_hd/__init__.py
+++ mne_pipeline_hd/__init__.py
@@ -5,8 +5,6 @@ License: BSD 3-Clause
 Github: https://github.com/marsipu/mne-pipeline-hd
 """
 
-from mne_pipeline_hd._version import __version__  # noqa: F401
-
 # Keep reference to Qt-objects without parent for tests
 # and to avoid garbage collection
 _object_refs = {
